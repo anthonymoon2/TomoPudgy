9209eb808e5ec40e8e1baa8b3f3d2df783ea1171
vluu11 <luuvuong11@gmail.com>
2024-11-14 15:04:11 -0800

    fixed login and register bugs

--- client/src/utils/mutations.ts
+++ client/src/utils/mutations.ts
@@ -6,12 +6,12 @@ export const REGISTER_USER = gql`
       _id
       username
       password
-      Weight
-      Height
-      Gender
-      Age
-      RecommendedCalorieCalculation
-      DailyCaloricIntake
+      weight
+      height
+      gender
+      age
+      recommendedCalorieCalculation
+      dailyCaloricIntake
     }
   }
 `;
@@ -22,12 +22,12 @@ export const LOGIN_USER = gql`
       _id
       username
       password
-      Weight
-      Height
-      Gender
-      Age
-      RecommendedCalorieCalculation
-      DailyCaloricIntake
+      weight
+      height
+      gender
+      age
+      recommendedCalorieCalculation
+      dailyCaloricIntake
     }
   }
 `;
