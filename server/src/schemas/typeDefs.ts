7138f5a7126285c3057c99367c1b727e40047866
Bob <bobjacob23@yahoo.com>
2024-11-14 11:27:34 -0800

    added food item model and type fooditem on typedefs

--- server/src/schemas/typeDefs.ts
+++ server/src/schemas/typeDefs.ts
@@ -7,12 +7,12 @@ const typeDefs = `
   _id: ID!
   username: String!
   password: String!
-  Weight: Float! 
-  Height: String!
-  Gender: Boolean!
-  Age: Int!
-  RecommendedCalorieCalculation: Float!
-  DailyCaloricIntake: Float!
+  weight: Float! 
+  height: String!
+  gender: Boolean!
+  age: Int!
+  recommendedCalorieCalculation: Float!
+  dailyCaloricIntake: Float!
 }
 
   type Mutation {
@@ -20,14 +20,22 @@ const typeDefs = `
   loginUser(username: String!, password: String! ): UserInfo
   addUserInfo(
     _id: ID!,
-    Weight: Float!,
-    Height: String!,
-    Gender: Boolean!,
-    Age: Int!,
-    RecommendedCalorieCalculation: Float!,
-    DailyCaloricIntake: Float!
+    weight: Float!,
+    height: String!,
+    gender: Boolean!,
+    age: Int!,
+    recommendedCalorieCalculation: Float!,
+    dailyCaloricIntake: Float!
   ): UserInfo
 }
+  type FoodItem {
+  _id: ID!
+  name: String!
+  calories: Float!
+  fat_total_g: Float!
+  protein_g: Float!
+  carbs_total_g: Float!
+  }
 `;
 
 export default typeDefs;
