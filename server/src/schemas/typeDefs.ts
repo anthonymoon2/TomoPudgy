9906ccac488a5b21cb65c9e2130bdd0244432363
joshcord99 <joshcord99@gmail.com>
2024-11-14 16:41:22 -0800

    fixed calculations

--- server/src/schemas/typeDefs.ts
+++ server/src/schemas/typeDefs.ts
@@ -1,38 +1,42 @@
 const typeDefs = `
   type Query {
-  getUserInfo(_id: String!): UserInfo
-  getFoodItem(name: String!): FoodItem
-}
+    getUserInfo(_id: String!): UserInfo
+    getFoodItem(name: String!): FoodItem
+    calculateUserCalories(_id: String, foodName: String):UserInfo
+  }
 
   type UserInfo {
-  _id: ID!
-  username: String!
-  password: String!
-  weight: Float! 
-  height: String!
-  gender: Boolean!
-  age: Int!
-  recommendedCalorieCalculation: Float!
-  dailyCaloricIntake: Float!
-}
+    _id: ID!
+    username: String!
+    password: String!
+    weight: Float!
+    height: String!
+    gender: Boolean!
+    age: Int!
+    recommendedCalorieCalculation: Float!
+    dailyCaloricIntake: Float!
+  }
 
   type Mutation {
-  createUser(username: String!, password: String!): UserInfo
-  loginUser(username: String!, password: String! ): UserInfo
-  addUserInfo(
-    _id: ID!,
-    weight: Float!,
-    height: String!,
-    gender: Boolean!,
-    age: Int!,
-    recommendedCalorieCalculation: Float!,
-    dailyCaloricIntake: Float!
-  ): UserInfo
-}
+    createUser(username: String!, password: String!): UserInfo
+    loginUser(username: String!, password: String!): UserInfo
+    addUserInfo(
+      _id: ID!
+      weight: Float!
+      height: String!
+      gender: Boolean!
+      age: Int!
+      recommendedCalorieCalculation: Float!
+      dailyCaloricIntake: Float!
+
+    ): UserInfo
+
+    
+  }
+
   type FoodItem {
-  name: String!
-  calories: Float!
+    name: String!
+    calories: Float!
   }
 `;
-
 export default typeDefs;
