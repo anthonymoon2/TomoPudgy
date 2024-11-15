72c4cf4463084240d63959fd03cc04c9a8f692b2
Anthony Moon <amoon@chapman.edu>
2024-11-15 13:27:11 -0800

    added component for home not complete

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -3,7 +3,18 @@ import AnimatedGifComponent from "../components/SpriteAnimation";
 const Home = () => {
     console.log('Rendering Home');
     return (
-        <AnimatedGifComponent />
+        <div className="grid grid-cols-[1fr_2fr] gap-[50px] m-[0px_50px] h-[500px]">
+            <div className="bg-black p-[2px]">
+                <div className="h-[500px] bg-customBeige border-[6px] border-solid border-amber-400 border-radius-[4px]">
+                    left
+                </div>
+            </div>
+            <div className="bg-black p-[2px]">
+                <div className="h-[500px] bg-customBeige border-[6px] border-solid border-amber-400">
+                    right
+                </div>
+            </div>
+        </div>
     )
     
 }
