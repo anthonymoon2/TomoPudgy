0f2b0e6179cdbd0a1970529ab17a25f51b992ee1
Bob <bobjacob23@yahoo.com>
2024-11-15 19:19:02 -0800

    got it to working!

--- server/src/schemas/typeDefs.ts
+++ server/src/schemas/typeDefs.ts
@@ -1,65 +1,58 @@
 const typeDefs = `
   type Query {
     getUserInfo(_id: String!): UserInfo
-    getFoodItem(name: String!): FoodItem
+    getFoodItem(foodName: String!): FoodItem
     calculateUserCalories(_id: String!, foodName: String!): Float
-  }
-
-  type UserInfo {
-    _id: ID!
-    username: String!
-    password: String!
-    weight: Float
-    feet: Int
-    inches: Int
-    gender: Boolean
+    }
+    
+    type UserInfo {
+      _id: ID!
+      username: String!
+      password: String!
+      weight: Float
+      feet: Int
+      inches: Int
+      gender: Boolean
     age: Int
     recommendedCalorieCalculation: Float
     dailyCaloricIntake: Float
     currentCalories: Float
-  }
-
-  type Compare {
-    result: Boolean
-    currentCalories: Float  
-  }
-
-  type Mutation {
-    createUser(username: String!, password: String!): UserInfo
-    loginUser(username: String!, password: String! ): UserInfo
-    addUserInfo(
-      _id: ID!,
-      weight: Float,
-      feet: Int,
-      inches: Int,
-      gender: Boolean,
+    foodItems: [FoodItem!]!
+    }
+    
+    type Compare {
+      result: Boolean
+      currentCalories: Float  
+      }
+      
+      type FoodItem {
+        name: String!
+        calories: Float!
+        _id: ID!
+        }
+        
+        type Mutation {
+          createUser(username: String!, password: String!): UserInfo
+          loginUser(username: String!, password: String! ): UserInfo
+          addUserInfo(
+            _id: ID!,
+            weight: Float,
+            feet: Int,
+            inches: Int,
+            gender: Boolean,
       age: Int
-    ): UserInfo
-    recommendedCalorieCalculation(
+      ): UserInfo
+      recommendedCalorieCalculation(
       _id: ID!,
       weight: Float!,
       feet: Int!,
       inches: Int!,
       age: Int!,
       gender: Boolean!
-    ): Float
-    compareUserCalories(_id: String!, foodName: String!): Compare
-  }
-
-  type FoodItem {
-    name: String
-    calories: Float
-    _id: ID!
-    username: String!
-    password: String!
-    weight: Float!
-    height: String!
-    gender: Boolean!
-    age: Int!
-    recommendedCalorieCalculation: Float!
-    dailyCaloricIntake: Float!
-    currentCalories: Float
-  }
-`;
+      ): Float
+      compareUserCalories(_id: String!): Compare
+      addFoodItemToUser(userId: String!, foodName: String!): FoodItem
+      }
+      `;
 
 export default typeDefs;
