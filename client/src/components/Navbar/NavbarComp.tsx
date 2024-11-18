64136e1d6a0093d74c9a7a4f166e819fee647068
vluu11 <luuvuong11@gmail.com>
2024-11-18 12:39:48 -0800

    added function to delete token on logout

--- client/src/components/Navbar/NavbarComp.tsx
+++ client/src/components/Navbar/NavbarComp.tsx
@@ -1,7 +1,13 @@
 import { NavLink } from "react-router-dom";
 import "../../styles/App.css";
+import AuthService from "../../utils/auth.js"
 
 const Navbar = () => {
+
+  const handleLogout = () => {
+    AuthService.logout(); // Call the logout method
+  };
+
   return (
     <nav className="navbarStyle flex justify-between text-4xl tracking-[.75rem]">
       <div className="text-5xl">
@@ -16,7 +22,7 @@ const Navbar = () => {
         <NavLink to="/profile" className="navbarLink">
           Profile
         </NavLink>
-        <NavLink to="/" className="navbarLink">
+        <NavLink to="/" className="navbarLink" onClick={handleLogout}>
           Logout
         </NavLink>
       </div>
