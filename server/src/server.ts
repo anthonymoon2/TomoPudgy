b75f777a58934d83a670e7ce87394707e0d4ff64
Kenjy Jap <kenhiebusiness@gmail.com>
2024-11-19 14:26:27 -0800

    update package.json and server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
 import { authenticateToken } from './utils/auth.js';
-import path from 'path';
+import path from 'node:path';
 import dotenv from 'dotenv';
 import cron from 'node-cron';
 import { resetCaloriesForAllUsers } from './utils/resetCaloriesAndFood.js';
@@ -11,6 +11,13 @@ dotenv.config();
 
 import { typeDefs, resolvers } from './schemas/index.js';
 import db from './config/connection.js';
+
+import {fileURLToPath} from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+
+// 👇️ "/home/john/Desktop/javascript"
+const __dirname = path.dirname(__filename);
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -44,10 +51,10 @@ const startApolloServer = async () => {
   ));
 
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/dist')));
+    app.use(express.static(path.join(__dirname, '../../client/dist')));
 
     app.get('*', (_req, res) => {
-      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+      res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
     });
   }
   
