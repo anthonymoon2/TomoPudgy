0fbab23518487b6c49a073609ec2aa5da3ae74c5
Kenjy Jap <kenhiebusiness@gmail.com>
2024-11-19 14:36:34 -0800

    fixed navbar hover

--- client/src/components/Navbar/NavbarComp.tsx
+++ client/src/components/Navbar/NavbarComp.tsx
@@ -35,7 +35,7 @@ const Navbar = () => {
             <li>
               <NavLink
                 to="/profile"
-                className="navbarLink block py-2 px-3 text-gray-900 rounded md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                className="navbarLink block py-2 px-3 text-gray-900 rounded md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white 0 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
                 Profile
               </NavLink>
